4b41f73f5a01fb86db5ae2952afe0b084cb9f249
bcreunited <94658596+acreunited@users.noreply.github.com>
2022-06-22 19:34:43 +0100

    mundo normal

--- IAV_FINAL/Assets/Scripts/Minecraft/Chunk.cs
+++ IAV_FINAL/Assets/Scripts/Minecraft/Chunk.cs
@@ -2,119 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk 
+public class Chunk
 {
     
     public Block[,,] chunkdata;
-
     public GameObject goChunk;
-
     Material material;
-
-    public enum ChunkStatus {DRAW,DONE};
+    public enum ChunkStatus { DRAW, DONE};
     public ChunkStatus status;
 
-    public Chunk(Vector3 pos,Material material)
-    {
+    public Chunk(Vector3 pos, Material material) {
         goChunk = new GameObject(World.CreateChunkName(pos));
         goChunk.transform.position = pos;
         this.material = material;
+
         BuildChunk();
     }
-    void BuildChunk()
-    {
+
+    void BuildChunk() {
         chunkdata = new Block[World.chunkSize, World.chunkSize, World.chunkSize];
-        for (int z = 0; z < World.chunkSize; z++)
-        {
-            for (int y = 0; y < World.chunkSize; y++)
-            {
-                for (int x = 0; x < World.chunkSize; x++)
-                {
+        for (int z = 0; z < World.chunkSize; z++) {
+            for (int y = 0; y < World.chunkSize; y++) {
+                for (int x = 0; x < World.chunkSize; x++) {
                     Vector3 pos = new Vector3(x, y, z);
-
-                 
-
                     int worldX = (int)goChunk.transform.position.x + x;
                     int worldY = (int)goChunk.transform.position.y + y;
                     int worldZ = (int)goChunk.transform.position.z + z;
-
-                    int h=Utils.GenerateHeight(worldX, worldZ);
-                    int hs= Utils.GenerateStoneHeight(worldX, worldZ);
+                    int h = Utils.GenerateHeight(worldX, worldZ);
+                    int hs = Utils.GenerateStoneHeight(worldX, worldZ);
 
                     if (worldY == h) {
-                        int random = Random.Range(0, 15);
-                        if (random > 5) {
-                            chunkdata[x, y, z] = new Block(Block.BlockType.GRASS, pos, this, material);
-                        }
-                        else {
-                            chunkdata[x, y, z] = new Block(Block.BlockType.DIRT, pos, this, material);
-                        }
+                        chunkdata[x, y, z] = new Block(Block.BlockType.GRASS, pos, this, material);
                     }
                     else {
                         chunkdata[x, y, z] = new Block(Block.BlockType.AIR, pos, this, material);
                     }
 
-                    /* if (worldY <= hs)
-                     {
-                         if(Utils.fBM3D(worldX,worldY,worldZ,1,0.5f)<0.495f) {
-                             if (Random.Range(0, 10)<=8) {
-                                 chunkdata[x, y, z] = new Block(Block.BlockType.STONE, pos, this, material);
-                             }
-                             else {
-                                 chunkdata[x, y, z] = new Block(Block.BlockType.DIAMOND, pos, this, material);
-                             }
-                         }  
-                         else {
-                             chunkdata[x, y, z] = new Block(Block.BlockType.AIR, pos, this, material);
-                         }
-                     }
-                     else if (worldY == h) {
-                         chunkdata[x, y, z] = new Block(Block.BlockType.GRASS, pos, this, material);
-                     }
-                     else if (worldY < h)
-                     {
-                         chunkdata[x, y, z] = new Block(Block.BlockType.DIRT, pos, this, material);
-                     }
-                     else
-                     {
-                         chunkdata[x, y, z] = new Block(Block.BlockType.AIR, pos, this, material);
-                     }*/
+                    //Debug.Log(Utils.fBM3D(worldX, worldY, worldZ, 1, 0.5f));
+                    //Grapher.Log(Utils.fBM3D(worldX, worldY, worldZ, 1, 0.5f), "fBM3D", Color.yellow);
 
-
+                    /*if (worldY <= hs) {
+                        if (Utils.fBM3D(worldX, worldY, worldZ, 1, 0.5f)<0.495f) {
+                            chunkdata[x, y, z] = new Block(Block.BlockType.STONE, pos, this, material);
+                        }
+                        else {
+                            chunkdata[x, y, z] = new Block(Block.BlockType.AIR, pos, this, material);
+                        }
+                    }
+                    else if (worldY==h) {
+                        chunkdata[x, y, z] = new Block(Block.BlockType.GRASS, pos, this, material);
+                    }
+                    else if (worldY < h) {
+                        chunkdata[x, y, z] = new Block(Block.BlockType.DIRT, pos, this, material);
+                    }
+                    else {
+                        chunkdata[x, y, z] = new Block(Block.BlockType.AIR, pos, this, material);
+                    }*/
                 }
-
             }
-
         }
         status = ChunkStatus.DRAW;
+
     }
 
     public void DrawChunk() {
-        for (int z = 0; z < World.chunkSize; z++)
-            for (int y = 0; y < World.chunkSize; y++)
-                for (int x = 0; x < World.chunkSize; x++)
-                    if (chunkdata[x, y, z]!=null) {
-                        chunkdata[x, y, z].Draw();
-                    }
-                    
-
+        for (int z = 0; z < World.chunkSize; z++) {
+            for (int y = 0; y < World.chunkSize; y++) {
+                for (int x = 0; x < World.chunkSize; x++) {
+                    chunkdata[x, y, z].Draw();
+                }
+            }
+        }
         CombineQuads();
-        MeshCollider collider =goChunk.AddComponent<MeshCollider>();
+        MeshCollider collider = goChunk.AddComponent<MeshCollider>();
         collider.sharedMesh = goChunk.GetComponent<MeshFilter>().mesh;
-        status=ChunkStatus.DONE;
-    
+        status = ChunkStatus.DONE;
     }
-    void CombineQuads()
-    {
+
+    void CombineQuads() {
         MeshFilter[] meshFilters = goChunk.GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 0;
-        while (i < meshFilters.Length)
-        {
+
+        while (i < meshFilters.Length) {
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
             i++;
         }
+
         MeshFilter mf = goChunk.AddComponent<MeshFilter>();
         mf.mesh = new Mesh();
 
@@ -123,11 +98,9 @@ public class Chunk
         MeshRenderer renderer = goChunk.AddComponent<MeshRenderer>();
         renderer.material = material;
 
-        foreach (Transform quad in goChunk.transform)
-        {
+        foreach (Transform quad in goChunk.transform) {
             GameObject.Destroy(quad.gameObject);
         }
     }
-    // Start is called before the first frame update
-    
+
 }
---
+++ IAV_FINAL/Assets/Scripts/Minecraft/part3/BlockInteraction.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInteraction : MonoBehaviour
+{
+    public Camera cam;
+    enum InteractionType { DESTROY, BUILD };
+    InteractionType interactionType;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool interaction = Input.GetMouseButtonDown(0) || Input.GetMouseButton(1);
+
+        if (interaction) {
+            interactionType = Input.GetMouseButtonDown(0) ? InteractionType.DESTROY : InteractionType.BUILD;
+            RaycastHit hit;
+
+           
+               
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 10)) {
+
+                if (hit.collider.gameObject.CompareTag("canRemove")) {
+                    Debug.Log("canRemove");
+                }
+
+                string chunkName = hit.collider.gameObject.name;
+                float chunkx = hit.collider.gameObject.transform.position.x;
+                float chunky = hit.collider.gameObject.transform.position.y;
+                float chunkz = hit.collider.gameObject.transform.position.z;
+
+                Vector3 hitBlock;
+                if (interactionType == InteractionType.DESTROY) {
+                    hitBlock = hit.point - hit.normal / 2f;
+                }
+                else {
+                    hitBlock = hit.point + hit.normal / 2f;
+                }
+
+                int blockx = (int) (Mathf.Round(hitBlock.x) - chunkx);
+                int blocky = (int)(Mathf.Round(hitBlock.y) - chunky);
+                int blockz = (int)(Mathf.Round(hitBlock.z) - chunkz);
+
+                Chunk c;
+                if (World.chunkDict.TryGetValue(chunkName, out c)) {
+                    if (interactionType == InteractionType.DESTROY && c.chunkdata[blockx, blocky, blockz].canRemove() ) {
+                        c.chunkdata[blockx, blocky, blockz].SetType(Block.BlockType.AIR);
+                    }
+                    else if (interactionType == InteractionType.BUILD) {
+                        c.chunkdata[blockx, blocky, blockz].SetType(Block.BlockType.STONE);
+                        c.chunkdata[blockx, blocky, blockz].setCanRemove(true);
+                    }
+                }
+
+                List<string> updates = new List<string>();
+                updates.Add(chunkName);
+                if (blockx==0) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx - World.chunkSize, chunky, chunkz)));
+                }
+                if (blockx == World.chunkSize-1) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx + World.chunkSize, chunky, chunkz)));
+                }
+                if (blocky == 0) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx - World.chunkSize, chunky, chunkz)));
+                }
+                if (blocky == World.chunkSize - 1) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx + World.chunkSize, chunky, chunkz)));
+                }
+                if (blockz == 0) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx - World.chunkSize, chunky, chunkz)));
+                }
+                if (blockz == World.chunkSize - 1) {
+                    updates.Add(World.CreateChunkName(new Vector3(chunkx + World.chunkSize, chunky, chunkz)));
+                }
+
+                foreach(string name in updates) {
+                    if (World.chunkDict.TryGetValue(name, out c)) {
+                        DestroyImmediate(c.goChunk.GetComponent<MeshFilter>());
+                        DestroyImmediate(c.goChunk.GetComponent<MeshRenderer>());
+                        DestroyImmediate(c.goChunk.GetComponent<MeshCollider>());
+                        c.DrawChunk();
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
